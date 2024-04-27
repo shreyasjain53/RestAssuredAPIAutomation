66ea3d408cb47c9ff91aa7c369e361923dafc9ed
Shreyas Kumar P <shreyas.kumar@o9solutions.com>
2024-04-27 19:19:32 +0530

    Added first request

--- src/test/java/com/rest/TestAPI.java
+++ src/test/java/com/rest/TestAPI.java
@@ -1,5 +1,10 @@
 package com.rest;
 
+import io.restassured.RestAssured;
+import io.restassured.RestAssured;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.config.SSLConfig;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
@@ -7,9 +12,23 @@ import static org.hamcrest.Matchers.*;
 
 public class TestAPI {
 
+
+
+
     @Test
     public void test(){
+        RestAssured.config = RestAssuredConfig.newConfig().sslConfig(SSLConfig.sslConfig().allowAllHostnames().relaxedHTTPSValidation());
+        baseURI = "https://api.postman.com";
+        String apiKey = "PMAK-661a536fb49c1e0001154e83-dc1f8a67515c9950da6cb73e7691580a5b";
 
-        given().when().then();
+        Response response = RestAssured.given()
+                .header("X-API-Key",apiKey)
+                .contentType("application/json").get("/workspaces");
+
+        System.out.println(response.getBody().asString());
+
+        System.out.println();
+
+        System.out.println("Status Code "+ response.getStatusCode());
     }
 }
