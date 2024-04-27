69e6a689f89a728f74fe1af4d2e8b7d2878b25da
Shreyas Kumar P <shreyas.kumar@o9solutions.com>
2024-04-27 21:45:35 +0530

    Added Base class for custom functions

--- src/test/java/com/rest/postman/GetPostManWorkSpace.java
+++ src/test/java/com/rest/postman/GetPostManWorkSpace.java
@@ -1,33 +1,32 @@
-package com.rest;
+package com.rest.postman;
 
-import io.restassured.RestAssured;
+import commonFunctions.Base;
 import io.restassured.RestAssured;
 import io.restassured.config.RestAssuredConfig;
 import io.restassured.config.SSLConfig;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
 
-public class TestAPI {
+import java.util.Properties;
 
+import static io.restassured.RestAssured.baseURI;
+
+public class GetPostManWorkSpace extends Base {
+
+ Properties data = loadProperties(GetPostManWorkSpace.class.getPackageName());
 
 
 
     @Test
-    public void test(){
+    public void getPostmanWorkspace(){
+        baseURI = data.getProperty("baseURI");
         RestAssured.config = RestAssuredConfig.newConfig().sslConfig(SSLConfig.sslConfig().allowAllHostnames().relaxedHTTPSValidation());
-        baseURI = "https://api.postman.com";
-        String apiKey = "PMAK-661a536fb49c1e0001154e83-dc1f8a67515c9950da6cb73e7691580a5b";
 
         Response response = RestAssured.given()
-                .header("X-API-Key",apiKey)
+                .header("X-API-Key",data.getProperty("apiKey"))
                 .contentType("application/json").get("/workspaces");
 
         System.out.println(response.getBody().asString());
-
-        System.out.println();
 
         System.out.println("Status Code "+ response.getStatusCode());
     }
