c58dfc299c08c418c8127aaf4c596e0894b7aff3
Shreyas Kumar P <shreyasjain53@gmail.com>
2024-05-04 16:03:42 +0530

    Updated build path and resolved Java class path issue

--- src/main/java/commonFunctions/Base.java
+++ src/main/java/commonFunctions/Base.java
@@ -3,31 +3,32 @@ package commonFunctions;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 public class Base {
 
-
-    /**
-     * Loads properties from a data.properties file located within the specified package.
-     *
-     * @param packageName The package name eg: YourClassName.class.getPackageName()
-     * @return Properties object containing the loaded properties.
-     */
-    public Properties loadProperties(String packageName) {
-        Properties prop = new Properties();
-        try {
-            String filePath = System.getProperty("user.dir")+ File.separator +"src"+File.separator+ "test"+File.separator+"java"+File.separator+ packageName.replace('.', File.separatorChar) + File.separator+ "data.properties";
-            System.out.println("data.properites file path :- "+ filePath);
-            FileInputStream fis = new FileInputStream(filePath);
-            prop.load(fis);
-            fis.close();
-        } catch (IOException e){
-           e.printStackTrace();
-        }finally {
-            System.gc();
-        }
-        return prop;
-    }
+	/**
+	 * Loads properties from a data.properties file located within the specified
+	 * package.
+	 *
+	 * @param packageName The package name eg: YourClassName.class.getPackageName()
+	 * @return Properties object containing the loaded properties.
+	 */
+	public Properties loadProperties(String packageName) {
+		Properties prop = new Properties();
+		try {
+			String filePath = System.getProperty("user.dir") + File.separator + "src" + File.separator + "test"
+					+ File.separator + "java" + File.separator + packageName.replace('.', File.separatorChar)
+					+ File.separator + "data.properties";
+			System.out.println("data.properites file path :- " + filePath);
+			FileInputStream fis = new FileInputStream(filePath);
+			prop.load(fis);
+			fis.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			System.gc();
+		}
+		return prop;
+	}
 }
--- src/test/java/com/rest/postman/GetPostManWorkSpace.java
+++ src/test/java/com/rest/postman/GetPostManWorkSpace.java
@@ -1,33 +1,32 @@
 package com.rest.postman;
 
+import static io.restassured.RestAssured.baseURI;
+
+import java.util.Properties;
+
+import org.testng.annotations.Test;
+
 import commonFunctions.Base;
 import io.restassured.RestAssured;
 import io.restassured.config.RestAssuredConfig;
 import io.restassured.config.SSLConfig;
 import io.restassured.response.Response;
-import org.testng.annotations.Test;
-
-import java.util.Properties;
-
-import static io.restassured.RestAssured.baseURI;
 
 public class GetPostManWorkSpace extends Base {
 
- Properties data = loadProperties(GetPostManWorkSpace.class.getPackageName());
+	Properties data = loadProperties(GetPostManWorkSpace.class.getPackageName());
 
+	@Test
+	public void getPostmanWorkspace() {
+		baseURI = data.getProperty("baseURI");
+		RestAssured.config = RestAssuredConfig.newConfig()
+				.sslConfig(SSLConfig.sslConfig().allowAllHostnames().relaxedHTTPSValidation());
 
+		Response response = RestAssured.given().header("X-API-Key", data.getProperty("apiKey"))
+				.contentType("application/json").get("/workspaces");
 
-    @Test
-    public void getPostmanWorkspace(){
-        baseURI = data.getProperty("baseURI");
-        RestAssured.config = RestAssuredConfig.newConfig().sslConfig(SSLConfig.sslConfig().allowAllHostnames().relaxedHTTPSValidation());
+		System.out.println(response.getBody().asString());
 
-        Response response = RestAssured.given()
-                .header("X-API-Key",data.getProperty("apiKey"))
-                .contentType("application/json").get("/workspaces");
-
-        System.out.println(response.getBody().asString());
-
-        System.out.println("Status Code "+ response.getStatusCode());
-    }
+		System.out.println("Status Code " + response.getStatusCode());
+	}
 }
